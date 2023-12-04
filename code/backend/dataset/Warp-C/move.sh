c4663ca78a7db9bbe4234a985bd2ce1c95553e44
rohitvaidyula <vaidyular@gmail.com>
2023-12-04 17:30:59 -0600

    New YOLOv8 Changes

--- code/backend/dataset/Warp-C/move.sh
+++
@@ -1,8 +0,0 @@
-mkdir all_train
-cd train_crops
-cp -r bottle/* canister/* cans/* cardboard/* detergent/* ../all_train
-cd ..
-
-mkdir all_test
-cd test_crops
-cp -r bottle/* canister/* cans/* cardboard/* detergent/* ../all_test
